ac98e107561e16f060bfa09b7e21e794dc39e3c5
martin <m.carregal@udc.es>
2023-07-21 17:23:12 +0200

    cambios menores

--- JS/Ejercicio6/script.js
+++ JS/Ejercicio6/script.js
@@ -1,20 +1,18 @@
-
 function anadirImagen(){
-    let miImagen = new Image(200, 300);
-    let miImagen2 = new Image(200, 300);
-    let miImagen3 = new Image(200, 300);
-    let miImagen4 = new Image(200, 300);
-    let miImagen5 = new Image(200, 300);
-
-    miImagen.src = 'imagenes/minion.png';
-    miImagen2.src = 'imagenes/minion2.png';
-    miImagen3.src = 'imagenes/minion3.png';
-    miImagen4.src = 'imagenes/minion4.png';
-    miImagen5.src = 'imagenes/minion5.jpeg';
-
-    document.body.appendChild(miImagen);
-    document.body.appendChild(miImagen2);
-    document.body.appendChild(miImagen3);
-    document.body.appendChild(miImagen4);
-    document.body.appendChild(miImagen5);
+    let div = document.getElementById("imagen");
+        for (var i = 1; i <= 5; i++) {
+            let img = div;
+            img = new Image(200,300);
+            img.src = "imagenes/minion5" + ".jpeg";
+            div.appendChild(img);
+        }
 }
+
+/*function eliminarImagen(){
+    var valor = document.getElementById("valor").value;
+    var imagenes = document.getElementById("imagen");
+    var imagen_a_eliminar = document.body.getElementsByClassName("imagen")[valor - 1];
+    imagenes.remove(imagen_a_eliminar);
+    //var imagen = imagenes.getElementsByTagName("img")[valor - 1];
+    //imagenes.removeChild(imagen);
+}*/
