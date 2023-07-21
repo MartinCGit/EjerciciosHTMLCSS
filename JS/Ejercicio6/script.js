892ac4ef35272aed1a5f73e169e6974eeb6eb4f9
martin <m.carregal@udc.es>
2023-07-21 12:11:28 +0200

    Terminado apartado b) ejercicio 6

--- JS/Ejercicio6/script.js
+++ JS/Ejercicio6/script.js
@@ -1,6 +1,20 @@
 
 function anadirImagen(){
     let miImagen = new Image(200, 300);
+    let miImagen2 = new Image(200, 300);
+    let miImagen3 = new Image(200, 300);
+    let miImagen4 = new Image(200, 300);
+    let miImagen5 = new Image(200, 300);
+
     miImagen.src = 'imagenes/minion.png';
+    miImagen2.src = 'imagenes/minion2.png';
+    miImagen3.src = 'imagenes/minion3.png';
+    miImagen4.src = 'imagenes/minion4.png';
+    miImagen5.src = 'imagenes/minion5.jpeg';
+
     document.body.appendChild(miImagen);
+    document.body.appendChild(miImagen2);
+    document.body.appendChild(miImagen3);
+    document.body.appendChild(miImagen4);
+    document.body.appendChild(miImagen5);
 }
